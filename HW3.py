2d5f425ae0fe30aebe83aaf85b97b1315d9bfac8
Nicole Giron <70114716+nicolegiron@users.noreply.github.com>
2021-11-02 22:38:40 -0400

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,408 @@
+# HW3
+#Due Date: 03/13/2021, 11:59PM
+
+"""
+### Collaboration Statement:
+
+"""
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return "Node({})".format(self.value)
+
+    __repr__ = __str__
+
+
+#=============================================== Part I ==============================================
+
+class Stack:
+    '''
+        >>> x=Stack()
+        >>> x.pop()
+        >>> x.push(2)
+        >>> x.push(4)
+        >>> x.push(6)
+        >>> x
+        Top:Node(6)
+        Stack:
+        6
+        4
+        2
+        >>> x.pop()
+        6
+        >>> x
+        Top:Node(4)
+        Stack:
+        4
+        2
+        >>> len(x)
+        2
+        >>> x.peek()
+        4
+    '''
+    def __init__(self):
+        self.top = None
+
+    def __str__(self):
+        temp=self.top
+        out=[]
+        while temp:
+            out.append(str(temp.value))
+            temp=temp.next
+        out='\n'.join(out)
+        return ('Top:{}\nStack:\n{}'.format(self.top,out))
+
+    __repr__=__str__
+
+
+    def isEmpty(self):
+        return self.top == None
+
+    def __len__(self):
+        l = []
+        top = self.top
+        while top != None:
+            l.append(top)
+            top = top.next
+        return len(l)
+
+
+    def push(self,value):
+        if self.isEmpty():
+            newNode = Node(value)
+            self.top = newNode
+        else:
+            newNode = Node(value)
+            newNode.next = self.top
+            self.top = newNode
+
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        else:
+            value = self.top.value
+            self.top = self.top.next
+            return value
+
+    def peek(self):
+        return self.top.value
+
+
+#=============================================== Part II ==============================================
+
+class Calculator:
+    def __init__(self):
+        self.__expr = None
+
+
+    @property
+    def getExpr(self):
+        return self.__expr
+
+    def setExpr(self, new_expr):
+        if isinstance(new_expr, str):
+            self.__expr=new_expr
+        else:
+            print('setExpr error: Invalid expression')
+            return None
+
+    def _isNumber(self, txt):
+        '''
+            >>> x=Calculator()
+            >>> x._isNumber(' 2.560 ')
+            True
+            >>> x._isNumber('7 56')
+            False
+            >>> x._isNumber('2.56p')
+            False
+        '''
+        try:
+            float(txt)
+        except ValueError:
+            return False
+        return True
+
+
+    def _getPostfix(self, txt):
+        '''
+            Required: _getPostfix must create and use a Stack for expression processing
+            >>> x=Calculator()
+            >>> x._getPostfix('2 ^ 4')
+            '2.0 4.0 ^'
+            >>> x._getPostfix('2')
+            '2.0'
+            >>> x._getPostfix('2.1 * 5 + 3 ^ 2 + 1 + 4.45')
+            '2.1 5.0 * 3.0 2.0 ^ + 1.0 + 4.45 +'
+            >>> x._getPostfix('2 * 5.34 + 3 ^ 2 + 1 + 4')
+            '2.0 5.34 * 3.0 2.0 ^ + 1.0 + 4.0 +'
+            >>> x._getPostfix('2.1 * 5 + 3 ^ 2 + 1 + 4')
+            '2.1 5.0 * 3.0 2.0 ^ + 1.0 + 4.0 +'
+            >>> x._getPostfix('( 2.5 )')
+            '2.5'
+            >>> x._getPostfix ('( ( 2 ) )')
+            '2.0'
+            >>> x._getPostfix ('2 * ( ( 5 + -3 ) ^ 2 + ( 1 + 4 ) )')
+            '2.0 5.0 -3.0 + 2.0 ^ 1.0 4.0 + + *'
+            >>> x._getPostfix ('( 2 * ( ( 5 + 3 ) ^ 2 + ( 1 + 4 ) ) )')
+            '2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
+            >>> x._getPostfix ('( ( 2 * ( ( 5 + 3 ) ^ 2 + ( 1 + 4 ) ) ) )')
+            '2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
+            >>> x._getPostfix('2 * ( -5 + 3 ) ^ 2 + ( 1 + 4 )')
+            '2.0 -5.0 3.0 + 2.0 ^ * 1.0 4.0 + +'
+
+            # In invalid expressions, you might print an error message, but code must return None, adjust doctest accordingly
+            # If you are veryfing the expression in calculate before passing to postfix, this cases are not necessary
+
+            >>> x._getPostfix('2 * 5 + 3 ^ + -2 + 1 + 4')
+            >>> x._getPostfix('2 * 5 + 3 ^ - 2 + 1 + 4')
+            >>> x._getPostfix('2    5')
+            >>> x._getPostfix('25 +')
+            >>> x._getPostfix(' 2 * ( 5 + 3 ) ^ 2 + ( 1 + 4 ')
+            >>> x._getPostfix(' 2 * ( 5 + 3 ) ^ 2 + ) 1 + 4 (')
+            >>> x._getPostfix('2 * 5% + 3 ^ + -2 + 1 + 4')
+        '''
+
+        postfixStack = Stack()
+        postfix = ""
+        operators = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
+        invalid = []
+        open, close, numbers, operator = 0, 0, 0, 0
+        for value in txt.split():
+            invalid.append(value)
+        for value in range(len(invalid)-1):
+            if invalid[value] in '+-*/^':
+              if invalid[value+1] in '+-*/^':
+                return None
+        for value in range(len(invalid)):
+            if invalid[value] == '(':
+                open +=1
+            elif invalid[value] == ')':
+                close +=1
+            elif self._isNumber(invalid[value]):
+                numbers += 1
+            elif invalid[value] in '+-*/^':
+                operator += 1
+        if open != close or numbers != operator+1:
+            return None
+
+        for value in txt.split():
+            if self._isNumber(value):
+                postfix += str(float(value)) + " "
+            elif value == "(":
+                postfixStack.push(value)
+            elif value == ")":
+                while not postfixStack.isEmpty() and postfixStack.peek() != "(":
+                    postfix += str(postfixStack.pop()) + " "
+                postfixStack.pop()
+            else:
+                if value not in operators:
+                    return None
+                if not postfixStack.isEmpty() and value == "^" and postfixStack.peek() == "^":
+                    postfixStack.push(value)
+                else:
+                    while not postfixStack.isEmpty() and postfixStack.peek() != "(" and operators[value] <= operators[postfixStack.peek()] :
+                        postfix += str(postfixStack.pop()) + " "
+                    postfixStack.push(value)
+        while not postfixStack.isEmpty():
+            if postfixStack.peek() in "()":
+                return None
+            postfix += str(postfixStack.pop()) + " "
+        return postfix.rstrip()
+
+
+
+    @property
+    def calculate(self):
+        '''
+            Required: calculate must call postfix
+                      calculate must create and use a Stack to compute the final result as shown in the video lecture
+            >>> x=Calculator()
+            >>> x.setExpr('4 + 3 - 2')
+            >>> x.calculate
+            5.0
+            >>> x.setExpr('-2 + 3.5')
+            >>> x.calculate
+            1.5
+            >>> x.setExpr('4 + 3.65 - 2 / 2')
+            >>> x.calculate
+            6.65
+            >>> x.setExpr('23 / 12 - 223 + 5.25 * 4 * 3423')
+            >>> x.calculate
+            71661.91666666667
+            >>> x.setExpr(' 2 - 3 * 4')
+            >>> x.calculate
+            -10.0
+            >>> x.setExpr('7 ^ 2 ^ 3')
+            >>> x.calculate
+            5764801.0
+            >>> x.setExpr(' 3 * ( ( ( 10 - 2 * 3 ) ) )')
+            >>> x.calculate
+            12.0
+            >>> x.setExpr('8 / 4 * ( 3 - 2.45 * ( 4 - 2 ^ 3 ) ) + 3')
+            >>> x.calculate
+            28.6
+            >>> x.setExpr('2 * ( 4 + 2 * ( 5 - 3 ^ 2 ) + 1 ) + 4')
+            >>> x.calculate
+            -2.0
+            >>> x.setExpr(' 2.5 + 3 * ( 2 + ( 3.0 ) * ( 5 ^ 2 - 2 * 3 ^ ( 2 ) ) * ( 4 ) ) * ( 2 / 8 + 2 * ( 3 - 1 / 3 ) ) - 2 / 3 ^ 2')
+            >>> x.calculate
+            1442.7777777777778
+
+
+            # In invalid expressions, you might print an error message, but code must return None, adjust doctest accordingly
+            >>> x.setExpr(" 4 + + 3 + 2")
+            >>> x.calculate
+            >>> x.setExpr("4  3 + 2")
+            >>> x.calculate
+            >>> x.setExpr('( 2 ) * 10 - 3 * ( 2 - 3 * 2 ) )')
+            >>> x.calculate
+            >>> x.setExpr('( 2 ) * 10 - 3 * / ( 2 - 3 * 2 )')
+            >>> x.calculate
+            >>> x.setExpr(' ) 2 ( * 10 - 3 * ( 2 - 3 * 2 ) ')
+            >>> x.calculate
+
+            # For extra credit only. If not attemped, these cases must return None
+            >>> x.setExpr('( 3.5 ) ( 15 )')
+            >>> x.calculate
+            >>> x.setExpr('3 ( 5 ) - 15 + 85 ( 12 )')
+            >>> x.calculate
+            >>> x.setExpr("( -2 / 6 ) + ( 5 ( ( 9.4 ) ) )")
+            >>> x.calculate
+        '''
+
+
+        if not isinstance(self.__expr,str) or len(self.__expr)<=0:
+            print("Argument error in calculate")
+            return None
+
+        calcStack = Stack()   # method must use calcStack to compute the  expression
+
+        post = self._getPostfix(self.__expr)
+        if post == None:
+            return None
+        for value in post.split():
+            if self._isNumber(value):
+                calcStack.push(value)
+            else:
+                num1 = calcStack.pop()
+                num2 = calcStack.pop()
+                if value == "+":
+                    calcStack.push(float(num2) + float(num1))
+                elif value == "-":
+                    calcStack.push(float(num2) - float(num1))
+                elif value == "*":
+                    calcStack.push(float(num2) * float(num1))
+                elif value == "/":
+                    calcStack.push(float(num2) / float(num1))
+                elif value == "^":
+                    calcStack.push(float(num2) ** float(num1))
+        return calcStack.pop()
+
+#=============================================== Part III ==============================================
+
+class AdvancedCalculator:
+    '''
+        >>> C = AdvancedCalculator()
+        >>> C.states == {}
+        True
+        >>> C.setExpression('a = 5;b = 7 + a;a = 7;c = a + b;c = a * 0;return c')
+        >>> C.calculateExpressions() == {'a = 5': {'a': 5.0}, 'b = 7 + a': {'a': 5.0, 'b': 12.0}, 'a = 7': {'a': 7.0, 'b': 12.0}, 'c = a + b': {'a': 7.0, 'b': 12.0, 'c': 19.0}, 'c = a * 0': {'a': 7.0, 'b': 12.0, 'c': 0.0}, '_return_': 0.0}
+        True
+        >>> C.states == {'a': 7.0, 'b': 12.0, 'c': 0.0}
+        True
+        >>> C.setExpression('x1 = 5;x2 = 7 * ( x1 - 1 );x1 = x2 - x1;return x2 + x1 ^ 3')
+        >>> C.states == {}
+        True
+        >>> C.calculateExpressions() == {'x1 = 5': {'x1': 5.0}, 'x2 = 7 * ( x1 - 1 )': {'x1': 5.0, 'x2': 28.0}, 'x1 = x2 - x1': {'x1': 23.0, 'x2': 28.0}, '_return_': 12195.0}
+        True
+        >>> print(C.calculateExpressions())
+        {'x1 = 5': {'x1': 5.0}, 'x2 = 7 * ( x1 - 1 )': {'x1': 5.0, 'x2': 28.0}, 'x1 = x2 - x1': {'x1': 23.0, 'x2': 28.0}, '_return_': 12195.0}
+        >>> C.states == {'x1': 23.0, 'x2': 28.0}
+        True
+        >>> C.setExpression('x1 = 5 * 5 + 97;x2 = 7 * ( x1 / 2 );x1 = x2 * 7 / x1;return x1 * ( x2 - 5 )')
+        >>> C.calculateExpressions() == {'x1 = 5 * 5 + 97': {'x1': 122.0}, 'x2 = 7 * ( x1 / 2 )': {'x1': 122.0, 'x2': 427.0}, 'x1 = x2 * 7 / x1': {'x1': 24.5, 'x2': 427.0}, '_return_': 10339.0}
+        True
+        >>> C.states == {'x1': 24.5, 'x2': 427.0}
+        True
+        >>> C.setExpression('A = 1;B = A + 9;C = A + B;A = 20;D = A + B + C;return D - A')
+        >>> C.calculateExpressions() == {'A = 1': {'A': 1.0}, 'B = A + 9': {'A': 1.0, 'B': 10.0}, 'C = A + B': {'A': 1.0, 'B': 10.0, 'C': 11.0}, 'A = 20': {'A': 20.0, 'B': 10.0, 'C': 11.0}, 'D = A + B + C': {'A': 20.0, 'B': 10.0, 'C': 11.0, 'D': 41.0}, '_return_': 21.0}
+        True
+        >>> C.states == {'A': 20.0, 'B': 10.0, 'C': 11.0, 'D': 41.0}
+        True
+        >>> C.setExpression('A = 1;B = A + 9;2C = A + B;A = 20;D = A + B + C;return D + A')
+        >>> C.calculateExpressions() is None
+        True
+        >>> C.states == {}
+        True
+    '''
+
+    def __init__(self):
+        self.expressions = ''
+        self.states = {}
+
+    def setExpression(self, expression):
+        self.expressions = expression
+        self.states = {}
+
+    def _isVariable(self, word):
+        '''
+            >>> C = AdvancedCalculator()
+            >>> C._isVariable('volume')
+            True
+            >>> C._isVariable('4volume')
+            False
+            >>> C._isVariable('volume2')
+            True
+            >>> C._isVariable('vol%2')
+            False
+        '''
+        if isinstance(word, str) and len(word) > 0:
+            if word[0].isalpha() and word.isalnum():
+                return True
+        return False
+
+
+    def _replaceVariables(self, expr):
+        '''
+            >>> C = AdvancedCalculator()
+            >>> C.states = {'x1': 23.0, 'x2': 28.0}
+            >>> C._replaceVariables('1')
+            '1'
+            >>> C._replaceVariables('105 + x')
+            >>> C._replaceVariables('7 * ( x1 - 1 )')
+            '7 * ( 23.0 - 1 )'
+            >>> C._replaceVariables('x2 - x1')
+            '28.0 - 23.0'
+        '''
+        split = expr.split()
+        for x in range(len(split)):
+            if self._isVariable(split[x]):
+                if split[x] in self.states:
+                    split[x] = str(self.states[split[x]])
+                else:
+                    return None
+        return " ".join(split)
+
+
+    def calculateExpressions(self):
+        self.states = {}
+        calc = Calculator()
+        d = {}
+        expression = self.expressions.strip().split(";")
+        for x in expression[:-1]:
+            var = x.split("=")[0].strip()
+            calc.setExpr(str(self._replaceVariables(x.split("=")[1])))
+            if calc.calculate != None:
+                self.states[var] = float(calc.calculate)
+            else:
+                self.states = {}
+                return None
+            d[x] = self.states.copy()
+        calc.setExpr(str(self._replaceVariables(expression[-1].split('return')[1])))
+        d['_return_'] = float(calc.calculate)
+        return d
